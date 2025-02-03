dd04cf0a2ce0bc3d83ddb739284bbdba17c75e8d
nainwal01 <131379813+nainwal01@users.noreply.github.com>
2025-02-03 09:52:41 +0530

    Update ques2.cpp

--- ques2.cpp
+++ ques2.cpp
@@ -5,25 +5,28 @@ void solve()
 {
   int n;
   cin>>n;
-  vectro<int>v(n);
+  vector<int>v(n);
   for(int &i:v)cin>>i;
   int key;
   cin>>key;
   int st =0;
   int end = n-1;
   int mid =0;
+  int cnt =0;
   while(st<=end)
   {
     mid = (st+end)/2;
     if(v[mid] == key)
     {
       cout<<"Present at"<<" "<<mid+1<<endl;
+      cout<<"Comparison"<<" "<<cnt<<endl;
       return ;
     }
     else if(v[mid]<key)
       st = mid + 1; 
     else
       end  =mid-1;
+      cnt++;
   }
   cout<<"Not Present"<<" "<<n<<endl;
 }
@@ -34,3 +37,4 @@ int main()
   while(t--) solve();
   return 0;
 }
+
