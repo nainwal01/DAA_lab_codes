de91cb7ccef8d4667f27f0b26996b895b8410498
nainwal01 <131379813+nainwal01@users.noreply.github.com>
2025-02-10 09:19:48 +0530

    Create week2_1.cpp

---
+++ week2_1.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int &i:v)cin>>i;
+    int x;
+    cin>>x;
+    int f,l;
+    int st =0, end = n-1;
+    while(st<=end)
+    {
+        int mid= (end+st)/2;
+        if(v[mid] == x)
+        {
+            cout<<"Present in Array"<<endl;
+            break;
+        }
+        else if(v[mid]<x)
+          st = mid+1;
+        else end = mid-1;
+    }
+    l = upper_bound(v.begin(),v.end() , x+1)-v.begin();
+    f = upper_bound(v.begin(), v.end() , x) -v.begin();
+    cout<<"Number of copies are "<<l-f<<endl;
+}
